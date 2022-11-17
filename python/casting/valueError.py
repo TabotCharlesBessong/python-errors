dca52d157a5f69d5aeb6eb58a589f3f9a148b04e
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2022-11-18 00:02:42 +0100

    Update valueError.py

--- python/casting/valueError.py
+++ python/casting/valueError.py
@@ -2,3 +2,24 @@
 
 error = {intTemp : int(row[1])}
 solution = {intTemp:int(float(row[1]))}
+
+# The error above sometimes happens when you are trying convert list of element into a form and some values cannot be tycast
+# for example it having some string string value instead of string num values
+# example
+day,temp,condition
+Monday,12,Sunny
+Tuesday,14,Rain
+Wednesday,15,Rain
+Thursday,14,Cloudy
+Friday,21,Sunny
+Saturday,22,Sunny
+Sunday,24,Sunny
+
+# In the first column , we have real string and not just numbers which can be strings 
+
+# so the best solution is shown below
+for row in d:
+    if row[1] != 'temp':
+      intTemp = int(row[1])
+      temperatures.append(intTemp)
+  print(temperatures)
