a101df3c2cbf5090078b0ea10d3d501d6ef166a2
Charles Bessong Tabot <82298650+TabotCharlesBessong@users.noreply.github.com>
2022-11-18 00:22:55 +0100

    Create pip.py

---
+++ thirdPackages/install/pip.py
@@ -0,0 +1,3 @@
+error = "pip is not recongnise as an internal or external commnad"
+# When you have this error , cd to the directory where your python is installed and right the command below
+solution = "python -m pip install -U PySide"
